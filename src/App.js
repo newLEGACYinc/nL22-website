2e08b3e69e55621d457c533ccacb8d658da9719b
nLspriter <52107943+GACastro95@users.noreply.github.com>
2022-06-30 15:50:29 +0100

    Initial Admin Panel Implementation

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import HoLApp from './higherorlower/HoLApp';
 import Homepage from './homepage/Home';
 import KayfableApp from './kayfable/KayfableApp';
+import AdminApp from './admin/AdminApp';
 import Header from './components/Navbar'
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route exact path="/" element={<Homepage />} />
         <Route exact path="/higherorlower" element={<HoLApp />} />
         <Route exact path="/kayfable" element={<KayfableApp />} />
+        <Route exact path="/admin" element={<AdminApp />} />
       </Routes>
     </div>
   );
